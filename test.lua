fd0758fa9915daad77abc5b5f2f8aff2b7e94033
张道瑄 <145451005+4amGodvzx@users.noreply.github.com>
2024-04-20 17:40:49 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,7 +2,11 @@ function  update()
     gcs:send_text(1,string.format("num of sensor:%f",gps:num_sensors()))
     local pri_sensor = gps:primary_sensor()
     local loc = gps:location(pri_sensor)
-    gcs:send_text(3,string.format("Recieve:%f,%f,%f",loc:lat(),loc:lng(),loc:alt()))
+    local loch = ahrs:get_position()
+    if loc ~= nil and loch ~= nil then
+        loch:change_alt_frame(1)
+        gcs:send_text(3,string.format("Recieve:%f,%f,%f",loc:lat(),loc:lng(),loch:alt()))
+    end
     return update,1000
 end
 return update,1000
