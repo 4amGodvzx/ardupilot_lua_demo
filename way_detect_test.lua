37ead827ef8491d5a8175ad1ebdb66ae4b01e997
张道瑄 <145451005+4amGodvzx@users.noreply.github.com>
2024-07-14 14:49:20 +0800

    Add files via upload

--- way_detect_test.lua
+++ way_detect_test.lua
@@ -1,11 +1,11 @@
 function update()
     local index = mission:get_current_nav_index()
     if index ~= nil then
-        gcs:send_text(0,string.format("index:%f",index))
+        gcs:send_text(6,string.format("index:%f",index))
         if index >= 18 then
             param:set_and_save("TARGET_WAYPOINT",1)
         else
-            return update,1000
+            return update,500
         end
     end
 end
