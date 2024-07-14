b528589c239954baad7d1579b3055bd2932d3888
张道瑄 <145451005+4amGodvzx@users.noreply.github.com>
2024-07-14 14:58:44 +0800

    Add files via upload

--- demo_to_test_new_version.lua
+++ demo_to_test_new_version.lua
@@ -8,8 +8,12 @@ local function create_parameter()
     param:add_param(PARAM_TABLE_KEY,6,"NUM",0)
 end
 local function target_location() --标靶信息传入模块
-    itargetloc = {22.590431,113.975327} --{纬度,经度}
-    return true
+    if param:get("TARGET_GET") == 1 then
+        itargetloc = {param:get("TARGET_LAT")/1e7,param:get("TARGET_LNG")/1e7} --{纬度,经度}
+        return true
+    else
+        return false
+    end
 end
 local function wait_for_waypoint_change() --等待飞机直线飞行
     if param:get("TARGET_WAYPOINT") == 1 then
@@ -18,8 +22,8 @@ local function wait_for_waypoint_change() --等待飞机直线飞行
         return false
     end
 end
-local function error_correction(init_velocity) --速度误差修正函数,用于处理飞机速度与水瓶速度的统计关系(待定)
-    return init_velocity
+local function vec_correction(init_velocity,t_in) --速度误差修正函数,用于处理飞机速度与水瓶速度的统计关系(待定)
+    return init_velocity - 4.8 * 1.3 * init_velocity * init_velocity * t_in / 700
 end
 local function haversineDistance(a, b) --Haversine经纬度换算法
     local earthRadius = 6371000 -- 地球半径
@@ -42,19 +46,19 @@ local function dropping_calculation() --投弹计算
     end
     loch:change_alt_frame(1)
     local relative_height = loch:alt() / 100
-    if relative_height <= 0 or velocity_vec:length() < 0 then
+    if relative_height <= 0 or velocity_vec:length() < 2 then
         return false
     end
     local g = 9.7997 --河北石家庄重力加速度
-    local time = math.sqrt(2 * relative_height / g)
-    local xoff = time * error_correction(velocity_vec:x())
-    local yoff = time * error_correction(velocity_vec:y())
+    local a = g - 4.8 * 1.3 * g * relative_height / 1400
+    local time = math.sqrt(2 * relative_height / a)
+    local xoff = time * vec_correction(velocity_vec:length(),time) * velocity_vec:x() / velocity_vec:length()
+    local yoff = time * vec_correction(velocity_vec:length(),time) * velocity_vec:y() / velocity_vec:length()
     locs:offset(xoff,yoff)
     local remaining_distance --如果现在投弹,落点与标靶的距离
-    remaining_distance = haversineDistance({x = locs:lat() / 1e7,y = locs:lng() / 1e7},{x = itargetloc[1],y = itargetloc[2]})
-    rd_output = remaining_distance
+    remaining_distance = haversineDistance({x = locs:lat() / 1e7,y = locs:lng() / 1e7},{x = itargetloc[1],y = itargetloc[2]}) + velocity_vec:length() * 0.05 / 2
     gcs:send_text(6,string.format("Remaning distance:%f",remaining_distance))
-    if math.abs(remaining_distance) < 20 then --投弹决策范围10米
+    if math.abs(remaining_distance) < 10 then --投弹决策范围10米
         return true
     else
         return false
@@ -87,13 +91,6 @@ function update()
             if time_to_drop == true then
                 servo_output() --控制舵机执行投弹操作
                 gcs:send_text(6,"Dropping complete!")
-                local file = io.open("testlog.txt","a")
-                local pri_sensor = gps:primary_sensor()
-                local vec = gps:velocity(pri_sensor)
-                local loc = ahrs:get_position()
-                loc:change_alt_frame(1)
-                file:write(string.format("Groundspeed:%.4f,Remaning distance:%f,Height:%.2f\n"),vec,rd_output,loc:alt()/100)
-                file:close()
                 param:set_and_save("TARGET_GET",0)
                 param:set_and_save("TARGET_WAYPOINT",0)
             else
