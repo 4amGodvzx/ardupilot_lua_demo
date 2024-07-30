fefaf3315e3da978b58515235d218808c1988866
张道瑄 <145451005+4amGodvzx@users.noreply.github.com>
2024-07-30 14:13:49 +0800

    Update way_detect.lua

--- way_detect.lua
+++ way_detect.lua
@@ -2,16 +2,22 @@ function update()
     local index = mission:get_current_nav_index()
     if index ~= nil then
         if param:get("TARGET_GET") == 1 then
-            if index == 5 then
-                param:set_and_save("TARGET_WAYPOINT",1)
-            end
-        end
-        if param:get("TARGET_NUM") == 5 then
-            if index == 6 then
-                param:set_and_save("TARGET_REMEDY",1)
+            gcs:send_text(6,string.format("index:%f",index))
+            if param:get("TARGET_NUM") == 1 then
+                if index >= 18 then
+                    param:set_and_save("TARGET_WAYPOINT",1)
+                end
+            elseif param:get("TARGET_NUM") == 2 then
+                if index >= 19 then
+                    param:set_and_save("TARGET_WAYPOINT",1)
+                end
+            elseif param:get("TARGET_NUM") == 3 then
+                if index >= 17 then
+                    param:set_and_save("TARGET_WAYPOINT",1)
+                end
             end
         end
     end
-    return update,1000
+    return update,700
 end
 return update,1000
