5a4463ac357863f77e9c7f6f6902652b6cea2100
张道瑄 <145451005+4amGodvzx@users.noreply.github.com>
2024-08-13 14:36:02 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,12 +1,9 @@
 function  update()
-    gcs:send_text(1,string.format("num of sensor:%f",gps:num_sensors()))
-    local pri_sensor = gps:primary_sensor()
-    local loc = gps:location(pri_sensor)
-    local loch = ahrs:get_position()
-    if loc ~= nil and loch ~= nil then
-        loch:change_alt_frame(1)
-        gcs:send_text(3,string.format("Recieve:%f,%f,%f",loc:lat(),loc:lng(),loch:alt()))
+    if param:get("SCR_USER1") == 1 then
+        vehicle:set_mode(10)
+        gcs:send_text(0,"ok")
+    else
+        return update,500
     end
-    return update,1000
 end
 return update,1000
