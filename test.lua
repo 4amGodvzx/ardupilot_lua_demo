af121837380dbbdd8a309affc3f833e0e5715139
张道瑄 <145451005+4amGodvzx@users.noreply.github.com>
2024-11-12 22:33:30 +0800

    Delete test.lua

--- test.lua
+++
@@ -1,9 +0,0 @@
-function  update()
-    if param:get("SCR_USER1") == 1 then
-        vehicle:set_mode(10)
-        gcs:send_text(0,"ok")
-    else
-        return update,500
-    end
-end
-return update,1000
