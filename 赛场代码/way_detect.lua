c568965973fd2e632e9b13ab719a46022617eb7f
张道瑄 <145451005+4amGodvzx@users.noreply.github.com>
2024-11-18 14:43:29 +0800

    Update way_detect.lua

--- 赛场代码/way_detect.lua
+++ 赛场代码/way_detect.lua
@@ -1,20 +1,11 @@
+--此脚本用于判断飞机是否从RTL模式改出
 function update()
-    local index = mission:get_current_nav_index()
+    local index = mission:get_current_nav_index() --获取当前正在前往的航点编号
     if index ~= nil then
         if param:get("TARGET_GET") == 1 then
             gcs:send_text(6,string.format("index:%f",index))
-            if param:get("TARGET_NUM") == 1 then
-                if index >= 1 then
-                    param:set_and_save("TARGET_WAYPOINT",1)
-                end
-            elseif param:get("TARGET_NUM") == 2 then
-                if index >= 1 then
-                    param:set_and_save("TARGET_WAYPOINT",1)
-                end
-            elseif param:get("TARGET_NUM") == 3 then
-                if index >= 1 then
-                    param:set_and_save("TARGET_WAYPOINT",1)
-                end
+            if index >= 1 then
+                param:set_and_save("TARGET_WAYPOINT",1) --与核心脚本文件通讯，启动投弹
             end
         end
     end
