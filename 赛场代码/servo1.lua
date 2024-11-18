663d213d187980594afde3422b6cdb228ffab1aa
张道瑄 <145451005+4amGodvzx@users.noreply.github.com>
2024-11-18 13:56:46 +0800

    Update servo1.lua

--- 赛场代码/servo1.lua
+++ 赛场代码/servo1.lua
@@ -1,5 +1,6 @@
+--此脚本用于在飞机准备起飞时锁定投弹舵机
 function update()
-    if vehicle:get_mode() == 5 then
+    if vehicle:get_mode() == 5 then --当飞行模式为FBWA时锁定舵机
         local servo_output_function = 0
 	    SRV_Channels:set_output_pwm(servo_output_function, 1170)
         gcs:send_text(0,"servo locked")
